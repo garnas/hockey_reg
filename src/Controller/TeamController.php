926be96e73f025d021bd02d32ffba09b6dbf9478
ansgar.poelking <merkmich@gmail.com>
2023-04-07 23:12:41 +0200

    Update TeamController.php

--- src/Controller/TeamController.php
+++ src/Controller/TeamController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class RegisterTeamController extends AbstractController
+class TeamController extends AbstractController
 {
     #[Route('/')]
     public function index(): Response
